7a41c95e317bb27c6d73dd450e3ca17f5569df16
Grant Williams <greatgrant116@gmail.com>
2021-01-20 11:06:29 +0100

    Refactored MainActivity.java

--- app/src/main/java/com/gralliams/servepro/MainActivity.java
+++ app/src/main/java/com/gralliams/servepro/MainActivity.java
@@ -1,14 +1,261 @@
-package com.gralliams.servepro;
+package com.gralliams.trukindroneprovider;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.gralliams.servepro.R;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+import androidx.webkit.WebSettingsCompat;
+import androidx.webkit.WebViewFeature;
 
 public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    private String webUrl = "https://trukin.com.au";
+    ProgressBar progressBarWeb;
+    RelativeLayout relativeLayout;
+    Button btnNoInternetConnection;
+    SwipeRefreshLayout swipeRefreshLayout;
+    LottieAnimationView loadingAnimation;
+    TextView connectionMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        webView =  findViewById(R.id.myWebView);
+        progressBarWeb =  findViewById(R.id.progressBar);
+        loadingAnimation = findViewById(R.id.animationView);
+        connectionMessage = findViewById(R.id.txtNoConnection);
+        makeToolBarInvisible();
+
+        btnNoInternetConnection = findViewById(R.id.btnNoConnection);
+        relativeLayout =  findViewById(R.id.relativeLayout);
+        swipeRefreshLayout =  findViewById(R.id.swipeRefreshLayout);
+
+        swipeRefreshLayout.setColorSchemeColors(Color.BLUE,Color.YELLOW,Color.GREEN);
+
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                checkConnection();
+            }
+        });
+
+
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowContentAccess(false);
+        webView.getSettings().setSupportZoom(false);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setCacheMode( WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.getSettings().setDomStorageEnabled(true);
+        checkConnection();
+
+
+
+
+        //Handles WebView SwipeUp Problem
+        webView.getViewTreeObserver().addOnScrollChangedListener(() -> {
+            if (webView.getScrollY() == 0) {
+                swipeRefreshLayout.setEnabled(true);
+            } else {
+                swipeRefreshLayout.setEnabled(false);
+            }
+        });
+
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+                webView.loadUrl("about:blank");
+                checkConnection();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingAnimation.setVisibility(View.VISIBLE);
+                makeToolBarInvisible();
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                super.onReceivedSslError(view, handler, error);
+                handler.cancel();
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                swipeRefreshLayout.setRefreshing(false);
+                loadingAnimation.cancelAnimation();
+                loadingAnimation.setVisibility(View.GONE);
+                showToolbar();
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+
+                progressBarWeb.setVisibility(View.INVISIBLE);
+                progressBarWeb.setProgress(newProgress);
+                loadingAnimation.setVisibility(View.VISIBLE);
+                loadingAnimation.playAnimation();
+
+                if(newProgress == 100){
+
+                    progressBarWeb.setVisibility(View.GONE);
+                    loadingAnimation.cancelAnimation();
+                    loadingAnimation.setVisibility(View.GONE);
+                    showToolbar();
+
+                }
+
+
+                super.onProgressChanged(view, newProgress);
+            }
+        });
+
+
+
+
+        btnNoInternetConnection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkConnection();
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Are you sure you want to Exit?")
+                    .setNegativeButton("No",null)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+
+                            finishAffinity();
+                        }
+                    }).show();
+        }
+    }
+
+    public void checkConnection(){
+
+        ConnectivityManager connectivityManager = (ConnectivityManager)
+                this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifi = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        NetworkInfo mobileNetwork = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+
+
+        if(wifi.isConnected()){
+            webView.loadUrl(webUrl);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+            loadingAnimation.cancelAnimation();
+            loadingAnimation.setVisibility(View.GONE);
+            makeToolBarInvisible();
+        }
+        else if (mobileNetwork.isConnected()){
+            webView.loadUrl(webUrl);
+            webView.setVisibility(View.VISIBLE);
+            relativeLayout.setVisibility(View.GONE);
+            loadingAnimation.cancelAnimation();
+            loadingAnimation.setVisibility(View.GONE);
+            makeToolBarInvisible();
+        }
+        else{
+            loadingAnimation.cancelAnimation();
+            loadingAnimation.setVisibility(View.GONE);
+            webView.setVisibility(View.GONE);
+            relativeLayout.setVisibility(View.VISIBLE);
+            makeToolBarInvisible();
+        }
+
+
+    }
+
+    private void makeToolBarInvisible() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+    }
+
+    private void showToolbar() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().show();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.action_modeLight:
+
+                if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+                    // Turning on the light mode
+                    WebSettingsCompat.setForceDark(webView.getSettings(), WebSettingsCompat.FORCE_DARK_OFF);
+                }
+                break;
+            case R.id.action_modedark:
+                if (WebViewFeature.isFeatureSupported(WebViewFeature.FORCE_DARK)) {
+                    // Turning on the dark mode
+                    WebSettingsCompat.setForceDark(webView.getSettings(), WebSettingsCompat.FORCE_DARK_ON);
+                }
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
